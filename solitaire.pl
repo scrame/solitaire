fe8336f8f5ca19dd240f3e4eaaa7d0872c059034
scrame <scrame@dev.scrame>
2010-02-06 22:01:08 -0800

    added shuffling algorithm.

--- solitaire.pl
+++ solitaire.pl
@@ -1,5 +1,8 @@
 #!/usr/bin/perl -l
 
+#NOTE: IF YOU WANT DEBUGGING INFORMATION, SET THE ENVIRONMENT VARIABLE DEBUG!
+
+
 use strict;
 use warnings;
 
@@ -12,9 +15,20 @@ my @suits = qw/D C H S/;
 my @ranks = qw/2 3 4 5 6 7 8 9 10 J Q K A/;
 
 
+##MAIN FUNCTION:
+
 #create a deck
 my @deck = create_deck();
+
 #shuffle the deck
+
+
+# from wikipedia: http://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
+#To shuffle an array a of m elements:
+#   initialize a such that . i . a[i] . i
+#   for n from m - 1 downto 1 do
+#         j . random (0 .. n)
+#         exchange a[j] and a[n]
 
 
 #create a playing field, it should have 7 slots. Each slot should have a reserve and an active card.
@@ -31,6 +45,8 @@ my @deck = create_deck();
 
 #print the debug info:
 print_debug();
+
+##END MAIN FUNCTION
 
 
 ##DECK FUNCTIONS
